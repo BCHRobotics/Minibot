c482622ecf28f871ebd4959b516eaac1270fbfe1
Devesh Kotak <kdevu1997@Gmail.com>
2024-12-19 16:00:55 -0500

    in progress

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,17 +7,25 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
+  private RobotContainer robotContainer;
+
+  private Timer timer;
 
  /** This method is called once when the robot starts up. */
 @Override
 public void robotInit() { 
    
+  robotContainer = new RobotContainer();
+  timer = new Timer(); //WPILib Timer
 }
 
 /** This method is called periodically, regardless of the robot mode. */
 @Override
 public void robotPeriodic() { 
+   CommandScheduler.getInstance().run();
    
+  this.robotContainer.getDrivetrain().getTemperature();
+  this.robotContainer.getDrivetrain().getBusVoltage();
 }
 
 /** This method is called once when teleop mode (driver control) starts. */
@@ -48,12 +56,25 @@ public void autonomousInit() {
 @Override
 public void disabledInit() { 
    
+robotContainer.getDrivetrain().brake();
+
+//Reset then Start the timer
+timer.reset();
+timer.start();
+
 }
+
 
 /** This method is called periodically during disabled mode. */
 @Override
 public void disabledPeriodic() { 
    
+      //Check if 3 seconds have elapsed
+  if (timer.hasElapsed(3.0)) {
+    //Switch to coast mode
+    robotContainer.getDrivetrain().releaseBrakes();
+    timer.stop(); //Stop the timer after switching to coast mode
+  }
 }
 
 }
