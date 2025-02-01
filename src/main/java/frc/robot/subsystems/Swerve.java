a21a289175dc6872e14f9c0ad6aad33efac555b9
Adam E <Adam.m.elsafty@gmail.com>
2025-02-01 08:58:20 -0500

    hmm weird

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,26 +1,22 @@
 import java.io.File;
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import edu.wpi.first.math.util.Units;
+
+
+double maximumSpeed = Units.feetToMeters(4.5)
+File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+SwerveDrive  swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
-
-double maximumSpeed = Units.feetToMeters(4.5);
-File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
-SwerveDrive  swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
-
-public class Swerve extends SubsystemBase{
-    /**
+  /**
    * Command to drive the robot using translative values and heading as a setpoint.
+   *
    * @param translationX Translation in the X direction.
    * @param translationY Translation in the Y direction.
    * @param headingX     Heading X to calculate angle of the joystick.
@@ -55,12 +51,11 @@ public class Swerve extends SubsystemBase{
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX)
   {
     return run(() -> {
-            // Make the robot move
-            swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumVelocity(),
+      // Make the robot move
+      swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumVelocity(),
                                           translationY.getAsDouble() * swerveDrive.getMaximumVelocity()),
                         angularRotationX.getAsDouble() * swerveDrive.getMaximumAngularVelocity(),
                         true,
                         false);
     });
   }
-}
