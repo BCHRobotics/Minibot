81a1a2043447ccceeb078021a4f81297f3e4e61c
Drake <drakemcgillivray@cmail.carleton.ca>
2025-02-08 08:48:45 -0500

    Test Branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,53 +1,36 @@
 package frc.robot;
 
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-//import frc.robot.subsystems.Drivetrain;
-
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.MoveElevatorCommand;
 import frc.robot.subsystems.Elevator;
-
-/** 
- * The RobotContainer is where we set up everything for the robot: 
- * 
- * - Creating Subsystem Objects (ex. drivetrain subsystem, elevator subystem) 
- *         NOTE: Jerry only has drivetrain subsystem 
- * 
- * - Creating controller for robot (xbox controller), and getting it's joystick values
- * 
- * - Button mappings (what buttons do what, ex. left bumper --> brake robot)
- * 
- */
+import frc.robot.Constants.ElevatorConstants;
 
 public class RobotContainer {
-
     private final Elevator elevator = new Elevator();
-
-    private final CommandXboxController driverController = new CommandXboxController(Constants.CONTROLLER.DRIVER_CONTROLLER_PORT);
-    
-
-    public Elevator getElevator() {
-        return elevator;
-    }
+    private final XboxController controller = new XboxController(Constants.CONTROLLER.DRIVER_CONTROLLER_PORT);
 
     public RobotContainer() {
         configureButtonBindings();
     }
 
     private void configureButtonBindings() {
-        this.driverController.a()
-        .onTrue(elevator.moveLevel("L1"));
-        this.driverController.b()
-        .onTrue(elevator.moveLevel("L2"));
-        this.driverController.y()
-        .onTrue(elevator.moveLevel("L3"));
-        this.driverController.x()
-        .onTrue(elevator.moveLevel("DOWN"));
+        // Example button bindings for elevator control
+        new Trigger(() -> controller.getAButton())
+            .onTrue(new MoveElevatorCommand(elevator, ElevatorConstants.L1));
+
+        new Trigger(() -> controller.getBButton())
+            .onTrue(new MoveElevatorCommand(elevator, ElevatorConstants.L2));
+
+        new Trigger(() -> controller.getXButton())
+            .onTrue(new MoveElevatorCommand(elevator, ElevatorConstants.L3));
+
+        new Trigger(() -> controller.getYButton())
+            .onTrue(new MoveElevatorCommand(elevator, ElevatorConstants.bottomPos));
     }
 
- 
-
-
-
+    public Elevator getElevator() {
+        return elevator;
+    }
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,166 +1,96 @@
 package frc.robot.subsystems;
-//import frc.robot.Constants;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.Command;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.DigitalInput;
-
 import frc.robot.Constants.ElevatorConstants;
 
+public class Elevator extends SubsystemBase {
 
-public class Elevator extends SubsystemBase{
-
-    private final SparkMax primaryMotor;
-    private final SparkMax followerMotor;
+    private final CANSparkMax primaryMotor;
+    private final CANSparkMax followerMotor;
     private final RelativeEncoder encoder;
     private final DigitalInput bottomLimit;
     private final PIDController pidController;
 
-    private double setpoint = 0;
+    private double setpoint = ElevatorConstants.downPos;
 
-    //private ElevatorPosition currentTarget = ElevatorPosition.DOWN;
-    
-    //SparkMaxConfig resetConfig = new SparkMaxConfig();
-    double currentPos;
-/* 
-    public enum ElevatorPosition {
-        DOWN(ElevatorConstants.downPos),
-        POSITION_1(ElevatorConstants.L1),
-        POSITION_2(ElevatorConstants.L2),
-        POSITION_3(ElevatorConstants.L3);
-
-        public final double positionInches;
-        
-        ElevatorPosition(double positionInches) {
-            this.positionInches = positionInches;
-        }
-    }
- */
     public Elevator() {
-        
-        primaryMotor = new SparkMax(ElevatorConstants.leftElevatorID, MotorType.kBrushless);
-        followerMotor = new SparkMax(ElevatorConstants.rightElevatorID, MotorType.kBrushless);
-
-        SparkMaxConfig followerConfig = new SparkMaxConfig();
-        //primaryMotor.setInverted(true);
-        followerConfig.follow(primaryMotor, true);
-
-        //configure motors
-        followerMotor.configure(followerConfig, null, null); 
-        
+        primaryMotor = new CANSparkMax(ElevatorConstants.leftElevatorID, MotorType.kBrushless);
+        followerMotor = new CANSparkMax(ElevatorConstants.rightElevatorID, MotorType.kBrushless);
         encoder = primaryMotor.getEncoder();
         bottomLimit = new DigitalInput(ElevatorConstants.limitSwitchPort);
-        
-        
+
+        followerMotor.follow(primaryMotor, true);
+
+        // Configure motors for safety and performance
+        primaryMotor.restoreFactoryDefaults();
+        primaryMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        primaryMotor.setSmartCurrentLimit(40);
+        primaryMotor.enableVoltageCompensation(12.0);
+
+        followerMotor.restoreFactoryDefaults();
+        followerMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        followerMotor.setSmartCurrentLimit(40);
+        followerMotor.enableVoltageCompensation(12.0);
+
+        // Set soft limits
+        primaryMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, (float) ElevatorConstants.topPos * (float) ElevatorConstants.countsPerInch);
+        primaryMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, (float) ElevatorConstants.bottomPos * (float) ElevatorConstants.countsPerInch);
+        primaryMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+        primaryMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+
         pidController = new PIDController(
             ElevatorConstants.ElevatorkP,
             ElevatorConstants.ElevatorkI,
             ElevatorConstants.ElevatorkD
         );
-         
         pidController.setTolerance(0.5);
-        
-        //setting limits for safety
-        SparkMaxConfig resetConfig = new SparkMaxConfig();
-        resetConfig.idleMode(IdleMode.kBrake);
-        resetConfig.smartCurrentLimit(40);
-        resetConfig.voltageCompensation(12.0);
-        
-        primaryMotor.configure(resetConfig, ResetMode.kResetSafeParameters, null);
-        followerMotor.configure(resetConfig, ResetMode.kResetSafeParameters, null);
-    
     }
-    
+
     private void handleBottomLimit() {
-        if(!bottomLimit.get()) {
+        if (!bottomLimit.get()) {
             stopMotors();
             encoder.setPosition(ElevatorConstants.bottomPos * ElevatorConstants.countsPerInch);
             setpoint = ElevatorConstants.bottomPos;
-            
-            if (setpoint == ElevatorConstants.bottomPos) {
-                pidController.reset();
+            pidController.reset();
         }
     }
-    } 
-    
+
     public void setTargetPosition(double positionInches) {
-        /* 
-        if (!bottomLimit.get()) {
-            setpoint = ElevatorConstants.bottomPos;
-        } else { }
-         */
-        setpoint = MathUtil.clamp(
-            positionInches, 
-            ElevatorConstants.bottomPos, 
-            ElevatorConstants.topPos);
+        setpoint = MathUtil.clamp(positionInches, ElevatorConstants.bottomPos, ElevatorConstants.topPos);
     }
-    
 
     public void stopMotors() {
         primaryMotor.set(0);
         pidController.reset();
     }
 
-
-    public void run() {
+    @Override
+    public void periodic() {
         double currentPosition = encoder.getPosition() / ElevatorConstants.countsPerInch;
-        
-        double pidOutput;
-        if (setpoint == ElevatorConstants.bottomPos) {
-            pidOutput = pidController.calculate(currentPosition, setpoint);
-        } else {
-            pidOutput = pidController.calculate(currentPosition, setpoint);
-        }
-        double feedForward = ElevatorConstants.feedForward*Math.signum(setpoint-currentPosition);
+
+        double pidOutput = pidController.calculate(currentPosition, setpoint);
+        double feedForward = ElevatorConstants.feedForward * Math.signum(setpoint - currentPosition);
         double output = pidOutput + feedForward;
 
         output = MathUtil.clamp(output, -ElevatorConstants.maxOutput, ElevatorConstants.maxOutput);
-        
         primaryMotor.set(output);
 
         handleBottomLimit();
 
-        System.out.println("Elevator Position" + currentPosition);
-        System.out.println("Elevator Setpoint" +  setpoint);
-        System.out.println("Elevator PID Output" + output);
-    }
-
-    
-    public Command moveLevel(String level) {
-        return runOnce(()-> {
-            switch (level) {
-                case "L1":
-                    setTargetPosition(ElevatorConstants.L1);
-                    break;
-                
-                case "L2":
-                    setTargetPosition(ElevatorConstants.L2);
-                    break;
-                
-                case "L3":
-                    System.out.println("Working");
-                    setTargetPosition(ElevatorConstants.L3);
-                    break;
-                case "DOWN":
-                    setTargetPosition(ElevatorConstants.bottomPos);
-                    break;
-            }
-        });//.andThen(() -> stopMotors());
+        SmartDashboard.putNumber("Elevator Position", currentPosition);
+        SmartDashboard.putNumber("Elevator Setpoint", setpoint);
+        SmartDashboard.putNumber("Elevator PID Output", output);
     }
 
     public boolean atSetpoint() {
         return pidController.atSetpoint();
     }
-
 }
-
